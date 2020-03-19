0e1ab2e4c171e3173a30e5f5ebfe09ce4a3e5b6e
cersaatrova <soteris100@gmail.com>
2020-03-19 03:20:57 +0200

    fix some bugs and create fast cash with jquery

--- display_game.php
+++ display_game.php
@@ -1,15 +1,61 @@
+<?php
+// session_start();
+if (isset($_POST['total'])) {
+    var_dump($_POST['total']);
+    // $amount = $_GET['amount'];
+
+}
+if (empty($_SESSION['poker'])) {
+    // $_SESSION['poker']=array();
+
+}
+// $amount=$_SESSION['poker']['amount'];
+?>
 <!DOCTYPE html>
 
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>5 Card Poker</title>
-   
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <link rel="stylesheet" href="style.css">
     <!-- font awesome library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    
+    <script>
+        $(document).ready(function() {
+            var amount = 0;
+            $("#amount").text(amount);
+            $("#frm-fast-cash").click(function() {
+                amount = 100 + parseInt($("#amount").text());
+                $({
+                    countNum: $('#amount').html()
+                }).animate({
+                    countNum: amount
+                }, {
+                    duration: 400,
+                    easing: 'linear',
+                    step: function() {
+                        $('#amount').html(Math.ceil(this.countNum));
+                    },
+                    complete: function() {
+                        $('#amount').html(amount);
+                        //alert('finished');
+                    }
+                });
+                $.ajax({
+                    type: 'GET',
+                    url: 'display_game.php',
+                    data: {
+                        total: amount
+                    }
+                
+                });
+
+            });
+        });
+    </script>
 </head>
+<?php var_dump($amount); ?>
 
 <body>
     <section class="game-body-cover">
@@ -18,13 +64,14 @@
             <a href="#"><i class="fa fa-info-circle" style="color: blue; font-size: 4rem "></i></a>
         </div>
         <!-- Button Fast Cash . on click add 100 chip on total amount of player chip -->
-        <form action="display_game.php" method="GET" id="frm-fast-cash">
+        <div id="frm-fast-cash">
             <div class="submit">
-                <input type="submit" value="Fast Cash" name="submit" class="fast-cash">
+                <a class="fast-cash href=">Fast Cash</a>
             </div>
-        </form>
+        </div>
         <!-- display the amount of the player -->
-        <p class="player-amount"><i class="fa fa-usd" aria-hidden="true"></i><?php echo $amount ?></p>
+        <p class="player-amount"><i class="fa fa-usd"></i><span id="amount"><?php echo $amount ?></span></p>
+
         <!-- close the game and print the result on a record on homepage -->
         <form action="index.php" method="GET" id="frm-cash-out">
             <div class="submit">
@@ -38,15 +85,15 @@
             <!-- here is all casino chips from 5 to 100  -->
             <div class="poker-chips-box">
                 <!-- casino chip rate of 100$ -->
-                <img class="chip" src="images/100.png" alt="chip 100">
+                <a href="display_game.php?chip=100&amount=<?php $amount ?>"> <img class="chip" src="images/100.png" alt="chip 100"></a>
                 <!-- casino chip rate of 50$ -->
-                <img class="chip" src="images/50.png" alt="chips 50">
+                <a href="display_game.php?chip=50"><img class="chip" src="images/50.png" alt="chips 50"></a>
                 <!-- casino chip rate of 20$ -->
-                <img class="chip" src="images/20.png" alt="chip 20" >
+                <a href="display_game.php?chip=20"><img class="chip" src="images/20.png" alt="chip 20"></a>
                 <!-- casino chip rate of 10$ -->
-                <img class="chip" src="images/10.png" alt="chip 10" >
+                <a href="display_game.php?chip=10"><img class="chip" src="images/10.png" alt="chip 10"></a>
                 <!-- casino chip rate of 5$ -->
-                <img class="chip" src="images/5.png" alt="chip 5" >
+                <a href="display_game.php?chip=5"><img class="chip" src="images/5.png" alt="chip 5"></a>
             </div>
 
             <!-- button of deal and showdown -->
