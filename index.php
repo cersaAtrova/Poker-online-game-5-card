beb19bec4f15ca4e243592d705dd819d6223fe6f
cersaatrova <soteris100@gmail.com>
2020-04-01 19:09:21 +0300

    fix bugs

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-    $_SESSION = array();
+$_SESSION = array();
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
+    <title>Asso Trantana - Casino</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/css?family=Oxygen&display=swap" rel="stylesheet">
@@ -30,9 +30,7 @@ session_start();
         }
         ?>
 
-
-
-
+    </div>
 
 
 
